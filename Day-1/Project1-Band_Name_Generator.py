0fdf17a559408df2a411cb44cc01b5e5283ed09d
Shaikh Mukhayer Malik <mukhayyermalikshaikh@gmail.com>
2024-09-10 13:17:23 +0530

    Add files via upload

---
+++ Day-1/Project1-Band_Name_Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator")
+City_Name = input("What's the name of city you grew up in?\n")
+Pet_Name = input("What's your Pet name?\n")
+Final_Name = City_Name + " " + Pet_Name
+print("Your band name could be " + Final_Name)
